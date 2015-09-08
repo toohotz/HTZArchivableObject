f018af6379f476e0ecb7913a6b46b6b1691f2727
Kemar White <gailsx@gmail.com>
2015-09-08 09:31:54 -0400

    Initial Commit

---
+++ HTZArchivableObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTZArchivableObject
+//
+//  Created by Kemar White on 9/8/15.
+//  Copyright © 2015 toohotz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
